d25eaee53c344d9d0c9f934d021945cb321f4ac5
AsifShaikh01 <asifshaikh0024@gmail.com>
2022-09-29 17:29:42 +0530

    mens page

---
+++ components/recentView.js
@@ -0,0 +1,14 @@
+const Recent = () =>{
+    return `
+    <div id="recent-top">
+    <button>Recently-Viewed</button>
+    <hr>
+</div>
+<div id="recent">
+    
+</div>
+<div id="showMore">
+    <button id="showMoreBtn">MORE</button>
+</div>`
+}
+export {Recent};
