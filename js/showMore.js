fd2e94344bf880398f0316caf70330b4b64d56ab
Rohini Shinde <shinderohini34@gmail.com>
2022-10-02 14:56:01 +0530

    file added

--- js/showMore.js
+++ js/showMore.js
@@ -17,17 +17,21 @@ const getData = async () =>{
 }
 getData();
 
+
 let beauty_div = document.querySelector("#beauty_images")
+
 const append = (actual_data) =>{
     beauty_div.innerHTML = null;
    actual_data.forEach((el)=>{
      let div = document.createElement("div");
+     div.className = "item"
      let icon = document.createElement("img");
      icon.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTbtpDKd3f8qcZsHqVkX1vzxFIDrSahCc1oQ&usqp=CAU";
      let poster = document.createElement("img");
      poster.src = el.link;
      let title = document.createElement("h3");
      title.innerText = el.name;
+     title.classList.add("results")
      let price = document.createElement("h4");
      price.innerText = "$"+el.price;
      let desc = document.createElement("p");
@@ -39,4 +43,33 @@ const append = (actual_data) =>{
      beauty_div.append(div);
    });
 }
+let filterinput = document.getElementById("filterinput");
+
+filterinput.addEventListener("keyup",filterProducts);
+
+function filterProducts(actual_data)
+{
+    let filterValue = filterinput.value.toUpperCase();
+
+    let item = beauty_div.querySelectorAll(".item");
+
+    for(let i=0;i<item.length;i++)
+    {
+        let span = item[i].name;
+
+        if(span.innerHTML.toUpperCase().indexOf(filterValue)>-1)
+        {
+            item[i].style.display = "initial";
+        }
+        else
+        {
+            item[i].style.display = "none";
+        }
+    }
+    //append(actual_data);
+    console.log(filterValue);
+}
+
+
+
 
